4364ee2a3c4c99701934f26aa874fa24461b8810
Brandon <Brandon@DESKTOP-29EF6T7>
2021-04-18 22:38:19 -0500

    Updates for more fields

--- uiTestingCsharp/Form1.Designer.cs
+++ uiTestingCsharp/Form1.Designer.cs
@@ -32,19 +32,25 @@ namespace uiTestingCsharp
             this.q1Label = new System.Windows.Forms.Label();
             this.q2Label = new System.Windows.Forms.Label();
             this.q1Answer = new System.Windows.Forms.TextBox();
-            this.q2Answer = new System.Windows.Forms.TextBox();
+            this.q3Answer = new System.Windows.Forms.TextBox();
             this.submit = new System.Windows.Forms.Button();
             this.info = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.q3Answer = new System.Windows.Forms.TextBox();
+            this.q4Answer = new System.Windows.Forms.TextBox();
+            this.q2Answer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.q5Answer = new System.Windows.Forms.TextBox();
+            this.gsdg = new System.Windows.Forms.Label();
+            this.q6Answer = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // q1Label
             // 
             this.q1Label.AutoSize = true;
             this.q1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.q1Label.Location = new System.Drawing.Point(13, 13);
+            this.q1Label.Location = new System.Drawing.Point(12, 16);
             this.q1Label.Name = "q1Label";
             this.q1Label.Size = new System.Drawing.Size(48, 18);
             this.q1Label.TabIndex = 0;
@@ -54,7 +60,7 @@ namespace uiTestingCsharp
             // 
             this.q2Label.AutoSize = true;
             this.q2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.q2Label.Location = new System.Drawing.Point(13, 56);
+            this.q2Label.Location = new System.Drawing.Point(12, 91);
             this.q2Label.Name = "q2Label";
             this.q2Label.Size = new System.Drawing.Size(108, 18);
             this.q2Label.TabIndex = 1;
@@ -67,12 +73,13 @@ namespace uiTestingCsharp
             this.q1Answer.Size = new System.Drawing.Size(272, 20);
             this.q1Answer.TabIndex = 2;
             // 
-            // q2Answer
+            // q3Answer
             // 
-            this.q2Answer.Location = new System.Drawing.Point(127, 54);
-            this.q2Answer.Name = "q2Answer";
-            this.q2Answer.Size = new System.Drawing.Size(212, 20);
-            this.q2Answer.TabIndex = 3;
+            this.q3Answer.Location = new System.Drawing.Point(127, 91);
+            this.q3Answer.Name = "q3Answer";
+            this.q3Answer.Size = new System.Drawing.Size(212, 20);
+            this.q3Answer.TabIndex = 3;
+            this.q3Answer.TextChanged += new System.EventHandler(this.q3Answer_TextChanged);
             // 
             // submit
             // 
@@ -92,7 +99,6 @@ namespace uiTestingCsharp
             this.info.Name = "info";
             this.info.Size = new System.Drawing.Size(0, 13);
             this.info.TabIndex = 5;
-            this.info.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // button2
             // 
@@ -109,30 +115,89 @@ namespace uiTestingCsharp
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 99);
+            this.label1.Location = new System.Drawing.Point(12, 127);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(97, 18);
             this.label1.TabIndex = 7;
             this.label1.Text = "Card Number";
             // 
-            // q3Answer
+            // q4Answer
             // 
-            this.q3Answer.Location = new System.Drawing.Point(116, 97);
-            this.q3Answer.Name = "q3Answer";
-            this.q3Answer.Size = new System.Drawing.Size(223, 20);
-            this.q3Answer.TabIndex = 8;
+            this.q4Answer.Location = new System.Drawing.Point(116, 128);
+            this.q4Answer.Name = "q4Answer";
+            this.q4Answer.Size = new System.Drawing.Size(223, 20);
+            this.q4Answer.TabIndex = 8;
+            // 
+            // q2Answer
+            // 
+            this.q2Answer.Location = new System.Drawing.Point(99, 54);
+            this.q2Answer.Name = "q2Answer";
+            this.q2Answer.Size = new System.Drawing.Size(240, 20);
+            this.q2Answer.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 18);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Billing Address";
+            // 
+            // q5Answer
+            // 
+            this.q5Answer.CausesValidation = false;
+            this.q5Answer.Location = new System.Drawing.Point(124, 165);
+            this.q5Answer.Name = "q5Answer";
+            this.q5Answer.Size = new System.Drawing.Size(215, 20);
+            this.q5Answer.TabIndex = 12;
+            // 
+            // gsdg
+            // 
+            this.gsdg.AutoSize = true;
+            this.gsdg.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gsdg.Location = new System.Drawing.Point(12, 206);
+            this.gsdg.Name = "gsdg";
+            this.gsdg.Size = new System.Drawing.Size(90, 18);
+            this.gsdg.TabIndex = 13;
+            this.gsdg.Text = "Postal Code";
+            // 
+            // q6Answer
+            // 
+            this.q6Answer.CausesValidation = false;
+            this.q6Answer.Location = new System.Drawing.Point(108, 204);
+            this.q6Answer.Name = "q6Answer";
+            this.q6Answer.Size = new System.Drawing.Size(231, 20);
+            this.q6Answer.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(351, 434);
-            this.Controls.Add(this.q3Answer);
+            this.ClientSize = new System.Drawing.Size(818, 434);
+            this.Controls.Add(this.q6Answer);
+            this.Controls.Add(this.gsdg);
+            this.Controls.Add(this.q5Answer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.q2Answer);
+            this.Controls.Add(this.q4Answer);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.info);
             this.Controls.Add(this.submit);
-            this.Controls.Add(this.q2Answer);
+            this.Controls.Add(this.q3Answer);
             this.Controls.Add(this.q1Answer);
             this.Controls.Add(this.q2Label);
             this.Controls.Add(this.q1Label);
@@ -148,12 +213,18 @@ namespace uiTestingCsharp
         private System.Windows.Forms.Label q1Label;
         private System.Windows.Forms.Label q2Label;
         private System.Windows.Forms.TextBox q1Answer;
-        private System.Windows.Forms.TextBox q2Answer;
+        private System.Windows.Forms.TextBox q3Answer;
         private System.Windows.Forms.Button submit;
         private System.Windows.Forms.Label info;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox q3Answer;
+        private System.Windows.Forms.TextBox q4Answer;
+        private System.Windows.Forms.TextBox q2Answer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox q5Answer;
+        private System.Windows.Forms.Label gsdg;
+        private System.Windows.Forms.TextBox q6Answer;
     }
 }
 
--- uiTestingCsharp/Form1.cs
+++ uiTestingCsharp/Form1.cs
@@ -22,37 +22,81 @@ namespace uiTestingCsharp
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-            Console.WriteLine("Hello");   
-        }
-
         private void submit_Click(object sender, EventArgs e)
         {
-            if (q1Answer.Text == "" || q2Answer.Text == "" || q3Answer.Text == "")
+            q3Answer.Text = q3Answer.Text.Replace(" ", String.Empty);
+            q4Answer.Text = q4Answer.Text.Replace(" ", String.Empty);
+            q6Answer.Text = q6Answer.Text.Replace(" ", String.Empty);
+            if (q1Answer.Text == "" || q2Answer.Text == "" || q3Answer.Text == "" || q4Answer.Text == "" || q5Answer.Text == "" || q6Answer.Text == "")
             {
                 info.Text = "Missing Required Information!";
             }
+            else if (q3Answer.Text.Length != 10)
+            {
+                info.Text = "This phone number is incorrect";
+            }
+            else if (q4Answer.Text.Length != 16)
+            {
+                info.Text = "This credit card information is incorrect";
+            }
+            else if (q6Answer.Text.Length != 6)
+            {
+                info.Text = "This postal code information is incorrect";
+            }
+
             else
             {
-                string line = q1Answer.Text + " " + q2Answer.Text + " " + q3Answer.Text;
-
-                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-                string docName = q1Answer.Text + ".txt";
-
-                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, docName)))
+                q6Answer.Text.ToUpper();
+                q6Answer.Text = q6Answer.Text.Replace(" ", String.Empty);
+                char[] az = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".ToCharArray();
+                char[] numberList = "1234567890".ToCharArray();
+                Console.WriteLine(q6Answer.Text);
+                if (!az.Contains(q6Answer.Text[0]))
                 {
-                    outputFile.WriteLine(line);
-                    Console.WriteLine("Written!");
-                    info.Text = "Saved Successfully";
+                    Console.WriteLine("1");
+                    info.Text = "This postal code information is incorrect";
+                }
+                else if (!numberList.Contains(q6Answer.Text[1]))
+                {
+                    Console.WriteLine("2");
+                    info.Text = "This postal code information is incorrect";
+                }
+                else if (!az.Contains(q6Answer.Text[2]))
+                {
+                    Console.WriteLine("3");
+                    info.Text = "This postal code information is incorrect";
+                }
+                else if (!numberList.Contains(q6Answer.Text[3]))
+                {
+                    Console.WriteLine("4");
+                    info.Text = "This postal code information is incorrect";
+                }
+                else if (!az.Contains(q6Answer.Text[4]))
+                {
+                    Console.WriteLine("5");
+                    info.Text = "This postal code information is incorrect";
+                }
+                else if (!numberList.Contains(q6Answer.Text[5]))
+                {
+                    Console.WriteLine("6");
+                    info.Text = "This postal code information is incorrect";
+                }
+                else
+                {
+                    string line = q1Answer.Text + "ʦ" + q2Answer.Text + "ʦ" + q3Answer.Text + "ʦ" + q4Answer.Text + "ʦ" + q5Answer.Text + "ʦ" + q6Answer.Text;
+
+                    string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                    string docName = q1Answer.Text + ".txt";
+
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, docName)))
+                    {
+                        outputFile.WriteLine(line);
+                        Console.WriteLine("Written!");
+                        info.Text = "Saved Successfully";
+                    }
                 }
             }
-        }
-
-        private void label1_Click_1(object sender, EventArgs e)
-        {
-
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -78,13 +122,21 @@ namespace uiTestingCsharp
                     using (StreamReader reader = new StreamReader(fileStream))
                     {
                         fileContent = reader.ReadToEnd();
-                        string[] words = fileContent.Split(' ');
+                        string[] words = fileContent.Split('ʦ');
                         q1Answer.Text = words[0];
                         q2Answer.Text = words[1];
                         q3Answer.Text = words[2];
+                        q4Answer.Text = words[3];
+                        q5Answer.Text = words[4];
+                        q6Answer.Text = words[5];
                     }
                 }
             }
+        }
+
+        private void q3Answer_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
